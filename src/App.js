c7d2af2af01ca80052ce5e56817de5395b5fb763
makennamartin97 <makennamartin97@yahoo.com>
2020-10-28 21:46:12 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const questions = [
+		{
+			questionText: 'Which country invented tea?',
+			answerOptions: [
+				{ answerText: 'England', isCorrect: false },
+				{ answerText: 'Japan', isCorrect: false },
+				{ answerText: 'China', isCorrect: true },
+				{ answerText: 'Thailand', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'What is your body’s largest organ?',
+			answerOptions: [
+				{ answerText: 'Heart', isCorrect: false },
+				{ answerText: 'Skin', isCorrect: true },
+				{ answerText: 'Liver', isCorrect: false },
+				{ answerText: 'Kidney', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which American state is the largest (by area)?',
+			answerOptions: [
+				{ answerText: 'Alaska', isCorrect: true },
+				{ answerText: 'Texas', isCorrect: false },
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'California', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who did Forbes name the youngest self-made billionaire in 2019?',
+			answerOptions: [
+				{ answerText: 'Justin Bieber', isCorrect: false },
+				{ answerText: 'Cardi B', isCorrect: false },
+				{ answerText: 'Taylor Swift', isCorrect: false },
+				{ answerText: 'Kylie Jenner', isCorrect: true },
+			],
+		},
+  ];
+  const [currentq, setcurrentq] = useState(0)
+  const [showscore, setshowscore] = useState(false)
+  const [score,setscore] = useState(0)
+
+
+  const handleclick = (isCorrect) => {
+    if(isCorrect === true){
+      setscore(score + 1);
+    }
+    const nextq = currentq + 1;
+
+    if(nextq < questions.length){
+      setcurrentq(nextq)
+    }else{
+      setshowscore(true)
+
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showscore ? (
+        <>
+        <div className="score-section">You scored {score} out of {questions.length}</div>
+
+        </>
+      ) : (
+        <>
+        <div className="q-section">
+          <div className="q-count">
+            <span>Question {currentq+1}</span>/{questions.length}
+          </div>
+          <div className="q-text">{questions[currentq].questionText}</div>
+        </div>
+        <div className="answer-section">
+          {questions[currentq].answerOptions.map((answerOption)=> (
+            <button className="btns"onClick={()=> handleclick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+          ))}
+        </div>
+        </>
+      )}
+      
     </div>
   );
 }
